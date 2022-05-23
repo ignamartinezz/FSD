868281270e882425e96ebc8fa99a73e12580e8c3
ignamartinezz <igna.martinezzz@gmail.com>
2022-05-23 20:31:15 -0300

    Avances en JS

---
+++ Sprint 2/Javascript/Main.js
@@ -0,0 +1,26 @@
+// let numero = 23;
+// let stringceano= "stringceano";
+// let verdad=true;
+// let decimal=10.2;
+// //constantes
+// const variable2=0;
+// numero= numero+1;
+// numero++;
+
+// console.log(verdad);
+// console.log(decimal);
+// // console.log("hola mundo");
+
+// // document.write(stringceano+numero);
+// let datos=document.getElementById("capoide");
+// datos.innerHTML= stringceano+numero;
+// // datos.innerHTML=`
+// // <h1>sos un capo<h1>
+// // `;
+// // alert(numero);
+// // alert(stringceano);
+// let insert= prompt("Ingrese CBU");
+let etiqueta = prompt("Cambiar etiqueta:");
+let newValor = prompt("Nuevo valor");
+const damevalor= document.getElementById("uno");
+damevalor.innerText= ("Algo nuevo");
